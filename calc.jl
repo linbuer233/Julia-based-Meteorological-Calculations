08c3a42fe0e829689fa89868dfa13cc5d1338119
我的二你模仿不来 <94788509+linbuer233@users.noreply.github.com>
2023-05-10 17:46:16 +0800

    v0.1

--- calc.jl
+++ calc.jl
@@ -1,14 +1,22 @@
 module calc
+    export e
     # 大气压强
     # https://latex.codecogs.com/svg.image?&space;P=P_0e^{-\frac{Mgh}{RT}}
     function atmospheric_pressure(P0, M, g, h, R, T)
         return P0 * exp(-M*g*h/(R*T))
     end
+    
+    # 水汽压
+    # https://latex.codecogs.com/svg.image?&space;e=6.11\times10%20^{\frac{7.5\times%20t}{237.3\times%20t}}
+    function e(temp)
+        return 6.11 * 10^((7.5 .* temp) ./ (237.3+ temp))
+    end
+    
     # 计算相对湿度 
     function relative_humidity(temp, dew_temp) # 单位为摄氏度 
-        es = 6.11 * 10^((7.5 .* temp) ./ (237.3+ temp)) # 饱和水汽压力
-        e = 6.11 * 10^((7.5 .* dew_temp) ./ (237.3+ dew_temp)) # 实际水汽压力
-        rh = e ./ es * 100 # 相对湿度
+        es = e(temp) # 饱和水汽压力
+        en = e(dew_temp) # 实际水汽压力
+        rh = en ./ es * 100 # 相对湿度
         return rh
     end    
 end
