6ebea3f4ab63ec2b7e8458db9aa8117b2f669cba
我的二你模仿不来 <94788509+linbuer233@users.noreply.github.com>
2023-05-11 12:02:39 +0800

    v0.1

--- calc.jl
+++ calc.jl
@@ -3,13 +3,14 @@ module calc
     # 大气压强
     # https://latex.codecogs.com/svg.image?&space;P=P_0e^{-\frac{Mgh}{RT}}
     function atmospheric_pressure(P0, M, g, h, R, T)
-        return P0 * exp(-M*g*h/(R*T))
+        return P0 .* exp(-M.* g .* h ./(R .* T))
     end
-    
+    #############################湿度特征参量#############################
     # 水汽压
     # https://latex.codecogs.com/svg.image?&space;e=6.11\times10%20^{\frac{7.5\times%20t}{237.3\times%20t}}
     function e(temp)
-        return 6.11 * 10^((7.5 .* temp) ./ (237.3+ temp))
+        # return 6.11 .* 10^((7.5 .* temp) ./ (237.3 .+ temp))
+        return 6.112 .* exp(17.67 .* temp./(temp+243.5))
     end
     
     # 计算相对湿度 
@@ -18,5 +19,35 @@ module calc
         en = e(dew_temp) # 实际水汽压力
         rh = en ./ es * 100 # 相对湿度
         return rh
-    end    
+    end
+
+    # 绝对湿度
+    function absolute_humidity(temp)
+        return 0.217 .* e(temp) ./ temp .* 100
+    end
+
+    # 比湿 （饱和比湿）
+    function q(temp,P)
+        return 622 .* e(temp) ./ (P .- 0.378 .* e(temp))  # 单位(g/kg)
+    end
+
+    # 混合比
+    function Wq(temp,P)
+        return 622 .* e(temp) ./ (P .-e(temp))
+    end
+
+    #############################温度特征参量#############################
+    # 位温
+    function theta(temp,P,P0)
+        return temp .* (P0 ./ P)^0.286
+    end
+
+    # 绝对虚温 
+    function temp_v(temp,q) # q为(g/g)
+        return (1 .+ 0.61 .* q).*temp
+    end
+    export q   
+    function temp_v(temp,P)
+        return (1 .+ 0.61 .* q(temp,P) ./1000) .* temp
+    end
 end
--- test.jl
+++ test.jl
@@ -4,6 +4,7 @@ using .calc
 
 ## 大气压强
 P0 = 101325
+P=80000
 M = 0.0289644 # 空气中主要是氮气和氧气，二者摩尔质量的平均值约为0.0289644 kg/mol
 g = 9.81
 h = 1000
@@ -11,12 +12,15 @@ R = 8.314
 T = 288
 
 a=calc.atmospheric_pressure(P0,M,g,h,R,T)
-println(a)
+println("ap\t",a)
 
 
 # 计算相对湿度
 
 temp=20
-dew_temp=20
+dew_temp=15
 
-println(calc.relative_humidity(temp,dew_temp))
+println("rh\t",calc.relative_humidity(temp,dew_temp))
+println("ah\t",calc.absolute_humidity(temp))
+println("q\t",calc.q(temp,P0/100))
+println("theta\t",calc.theta(temp,P,P0))
